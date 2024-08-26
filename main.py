2317740fc46b19a03f017619bd5279967a8597c5
Armaan Rana <85696385+armaanrana98@users.noreply.github.com>
2024-08-26 07:14:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import openai
 from openai import OpenAI
 from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings

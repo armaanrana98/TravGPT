eaa6da50fcbaa94f9af02050d71be4e5dde7535e
Armaan Rana <85696385+armaanrana98@users.noreply.github.com>
2024-09-23 14:21:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -89,7 +89,7 @@ def generate_answer(assistant_id, question):
 def main():
     st.header("TravGPT🤖")
 
-    question = st.text_input("Get answers to all your finance related queries:")
+    question = st.text_input("Get answers to all your finance and flight related queries:")
 
     if st.button("Ask"):
         with st.spinner("Processing..."):
